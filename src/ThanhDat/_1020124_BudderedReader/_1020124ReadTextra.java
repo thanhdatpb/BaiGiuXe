4c47ee76a5b9c2a19819524a45e546784c0bd9f4
Thanh Dat <tthanhdat.pb@gmail.com>
2024-04-05 16:36:56 +0700

    push project BaiGiuXe

--- src/ThanhDat/_1020124_BudderedReader/_1020124ReadTextra.java
+++ src/ThanhDat/_1020124_BudderedReader/_1020124ReadTextra.java
@@ -1,2 +1,43 @@
-package ThanhDat._1020124_BudderedReader;public class _1020124ReadTextra {
+package ThanhDat._1020124_BudderedReader;
+
+import lombok.Data;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+
+@Data
+public class _1020124ReadTextra {
+
+
+    private int LoaiXe;
+    private String BienSoXe;
+    private String SoVeXe;
+    private String ThoiGianRa;
+    private String TinhTrangXe;
+    public static void main(String[] args) throws IOException {
+        File vao = new File("D:\\Workspace\\Java\\BT Java\\BaiGiuXe\\ra.txt");
+//      Path p = new Path("D:\Workspace\Java\BT Java\BaiGiuXe\vao.txt");
+
+        try {
+            BufferedReader text = Files.newBufferedReader(vao.toPath(), StandardCharsets.UTF_8);
+            String line = null;
+            while (true){
+                line = text.readLine();
+                if (line==null){
+                    break;
+                }
+                else{
+                    System.out.println(line);
+                }
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/ThanhDat/_1020124_BudderedReader/_1020124ReadTextvao.java
+++ src/ThanhDat/_1020124_BudderedReader/_1020124ReadTextvao.java
@@ -1,2 +1,41 @@
-package ThanhDat._1020124_BudderedReader;public class _1020124ReadTextvao {
+package ThanhDat._1020124_BudderedReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+//import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+
+
+public class _1020124ReadTextvao {
+
+    private int LoaiXe;
+    private String BienSoXe;
+    private String SoVeXe;
+    private String ThoiGianVao;
+    private String TinhTrangXe;
+    public static void main(String[] args) throws IOException {
+        File vao = new File("D:\\Workspace\\Java\\BT Java\\BaiGiuXe\\vao.txt");
+//      Path p = new Path("D:\Workspace\Java\BT Java\BaiGiuXe\vao.txt");
+
+        try {
+            BufferedReader text = Files.newBufferedReader(vao.toPath(), StandardCharsets.UTF_8);
+            String line = null;
+            while (true){
+                line = text.readLine();
+                if (line==null){
+                    break;
+                }
+                else{
+                    System.out.println(line);
+                }
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
